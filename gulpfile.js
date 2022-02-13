978be00edf198167695f63d7f2a233233fab80ef
pMozze <ikMozze@gmail.com>
2022-02-13 14:04:59 +0300

    notebook grid

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ const browserSync = require('browser-sync').create();
 exports.default = () => {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: './',
         }
     });
 
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch('./*.html').on('change', browserSync.reload);
     gulp.watch('./assets/scss/**/*.scss', async () => {
         gulp.src('./assets/scss/**/*.scss').pipe(gulpPlumber()).pipe(gulpSASS()).pipe(gulp.dest('./')).pipe(browserSync.stream());
     });
