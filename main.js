a864f4da12d4c39fd672632b6598678635992fdb
pMozze <ikMozze@gmail.com>
2022-02-16 15:57:35 +0300

    grid fixes

--- main.js
+++ main.js
@@ -55,11 +55,11 @@ document.querySelectorAll('.holiday').forEach(holidayElement => {
             }
 
             if (event.currentTarget.classList.contains("holiday__button_next")) {
-                imageElement.setAttribute('src', '/assets/images/3.jpg');
+                imageElement.setAttribute('src', 'assets/images/3.jpg');
                 titleElement.textContent = 'Рождество Христово';
                 descriptionElement.textContent = 'Один из важнейших христианских праздников и государственный праздник в более чем 100 странах мира. В православии Рождество Христово празднуется 7 января';
             } else if (event.currentTarget.classList.contains("holiday__button_previus")) {
-                imageElement.setAttribute('src', '/assets/images/1.jpg');
+                imageElement.setAttribute('src', 'assets/images/1.jpg');
                 titleElement.textContent = 'Покров Святой Богородицы';
                 descriptionElement.textContent = 'В традиции русского православия праздник отмечается 1 (14) октября; в греческом православии празднуется 1 и 28 октября по новому стиль.';
             }
