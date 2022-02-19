b90889f1c1d7c138a31e6417c4844efca4f7d376
pMozze <ikMozze@gmail.com>
2022-02-19 12:52:14 +0300

    Первая страница мобильной версии и исправление ошибок адаптива под мониторы

--- main.js
+++ main.js
@@ -1,41 +1,51 @@
 const currentDate = new Date();
 
+const days = [
+    'Воскресенье',
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота'
+];
+
+const months = [
+    'Января',
+    'Февраля',
+    'Мара',
+    'Апреля',
+    'Мая',
+    'Июня',
+    'Июля',
+    'Августа',
+    'Сентября',
+    'Октября',
+    'Ноября',
+    'Декабря'
+];
+
 document.querySelectorAll('.today__day').forEach(element => {
     element.textContent = currentDate.getDate();
 });
 
 document.querySelectorAll('.today__day-name').forEach(element => {
-    element.textContent = [
-        'Воскресенье',
-        'Понедельник',
-        'Вторник',
-        'Среда',
-        'Четверг',
-        'Пятница',
-        'Суббота'
-    ][currentDate.getDay()];
+    element.textContent = days[currentDate.getDay()];
 });
 
 document.querySelectorAll('.today__month').forEach(element => {
-    element.textContent = [
-        'Января',
-        'Февраля',
-        'Мара',
-        'Апреля',
-        'Мая',
-        'Июня',
-        'Июля',
-        'Августа',
-        'Сентября',
-        'Октября',
-        'Ноября',
-        'Декабря'
-    ][currentDate.getMonth()];
+    element.textContent = months[currentDate.getMonth()];
 });
 
-document.querySelectorAll('.holiday').forEach(holidayElement => {
+document.querySelectorAll('.today-touch').forEach(element => {
+    element.textContent += ` ${days[currentDate.getDay()]}, ${currentDate.getDate()} ${months[currentDate.getMonth()]}`;
+});
+
+document.querySelectorAll('.holiday, .holiday-touch').forEach(holidayElement => {
     const images = holidayElement.querySelectorAll(".holiday__image");
     const descriptions = holidayElement.querySelectorAll(".holiday__description");
+    const durations = holidayElement.querySelectorAll(".holiday__duration");
+    const buttons = holidayElement.querySelectorAll("a.holiday__button");
 
     const elementHandler = (element, elementIndex, slideIndex) => {
         console.log(element, elementIndex, slideIndex - 1);
@@ -54,10 +64,14 @@ document.querySelectorAll('.holiday').forEach(holidayElement => {
             if (event.currentTarget.classList.contains("holiday__button_next") && currentSlide + 1 <= images.length) {
                 images.forEach((image, imageIndex) => elementHandler(image, imageIndex, currentSlide + 1));
                 descriptions.forEach((description, descriptionIndex) => elementHandler(description, descriptionIndex, currentSlide + 1));
+                durations.forEach((duration, durationIndex) => elementHandler(duration, durationIndex, currentSlide + 1));
+                buttons.forEach((button, buttonIndex) => elementHandler(button, buttonIndex, currentSlide + 1));
                 holidayElement.setAttribute('data-slide', currentSlide + 1);
             } else if (event.currentTarget.classList.contains("holiday__button_previus") && currentSlide - 1 >= 1) {
                 images.forEach((image, imageIndex) => elementHandler(image, imageIndex, currentSlide - 1));
                 descriptions.forEach((description, descriptionIndex) => elementHandler(description, descriptionIndex, currentSlide - 1));
+                durations.forEach((duration, durationIndex) => elementHandler(duration, durationIndex, currentSlide - 1));
+                buttons.forEach((button, buttonIndex) => elementHandler(button, buttonIndex, currentSlide - 1));
                 holidayElement.setAttribute('data-slide', currentSlide - 1);
             }
         });
